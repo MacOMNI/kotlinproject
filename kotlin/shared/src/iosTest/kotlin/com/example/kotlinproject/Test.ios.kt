c9c680085c6f0681f8f659ec40ec1b7ff936ea0d
MacOMNI <414294494@qq.com>
2024-02-10 15:40:34 +0800

    #update kotlinproject

---
+++ kotlin/shared/src/iosTest/kotlin/com/example/kotlinproject/Test.ios.kt
@@ -0,0 +1,12 @@
+package com.example.kotlinproject
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class IosGreetingTest {
+
+    @Test
+    fun testExample() {
+        assertTrue(Greeting().greet().contains("iOS"), "Check iOS is mentioned")
+    }
+}
